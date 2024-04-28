a40bed3de97146270eaf46124cf51601b2b536f4
Arkov25 <Marko.Veljkovic25@gmail.com>
2024-04-28 21:52:02 +0200

    flake.nix change from 23.11 to unstable system.nix new programs

--- modules/system.nix
+++ modules/system.nix
@@ -84,7 +84,13 @@ in {
   # $ nix search wget
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+    wget
+    neofetch
+    git
+    alacritty
+    curl
+    nnn
+    nushell
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
