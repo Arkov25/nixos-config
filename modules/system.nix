8eb0ad655c813e65b48ed967123dbb736ab8513c
Arkov25 <Marko.Veljkovic25@gmail.com>
2024-04-28 20:14:03 +0200

    Remove duplicate User

--- modules/system.nix
+++ modules/system.nix
@@ -5,21 +5,6 @@
 }: let
   username = "marko";
 in {
-  # ============================= User related =============================
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.marko = {
-    isNormalUser = true;
-    description = "marko";
-    extraGroups = ["networkmanager" "wheel"];
-    #openssh.authorizedKeys.keys = [
-     # "ssh-ed25519 AAAAC3NzaCAAAAAAAAAAAAAAIJx3Sk20pLL1b2PPAAAAAAAAAAADrErq83xAAAAAAAAA user@"
-    #];
-  };
-  # given the users in this list the right to specify additional substituters via:
-  #    1. `nixConfig.substituers` in `flake.nix`
-  #    2. command line args `--options substituers http://xxx`
-  nix.settings.trusted-users = [username];
 
   # Set your time zone.
   time.timeZone = "Europe/Berlin";
