3122df7e94fca1651da533e0253029f065e23d2d
Arkov25 <Marko.Veljkovic25@gmail.com>
2024-04-28 21:55:33 +0200

    typo

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/release-unstable";
+    home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
