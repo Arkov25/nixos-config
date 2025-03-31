c436da8eea82c10af9b540e9ac584ab3b42fbbee
Arkov25 <Marko.Veljkovic25@gmail.com>
2025-03-31 23:48:59 +0200

    pipewire.security does not exist

--- modules/core/pipewire.nix
+++ modules/core/pipewire.nix
@@ -6,7 +6,6 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    security.rtkit.enable = true; #optional but recommended
     # lowLatency.enable = true;
   };
   hardware.alsa.enablePersistence = true;
