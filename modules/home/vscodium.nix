d10a6d3affba2f5f93382734622f8fc5d1606e0b
Arkov25 <th4lyhvtf@mozmail.com>
2025-04-03 00:00:28 +0200

    vscodium

--- modules/home/vscodium.nix
+++ modules/home/vscodium.nix
@@ -1,7 +1,46 @@
 { pkgs, ... }:
+let
+  jonathanharty.gruvbox-material-icon-theme =
+    pkgs.vscode-utils.buildVscodeMarketplaceExtension
+      {
+        mktplcRef = {
+          name = "gruvbox-material-icon-theme";
+          publisher = "JonathanHarty";
+          version = "1.1.5";
+          hash = "sha256-86UWUuWKT6adx4hw4OJw3cSZxWZKLH4uLTO+Ssg75gY=";
+        };
+      };
+in
 {
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
+    profiles.default = {
+      extensions = with pkgs.vscode-extensions; [
+        jnoortheen.nix-ide
+        arrterian.nix-env-selector
+        ms-vscode.cpptools
+        jdinhlife.gruvbox
+        jonathanharty.gruvbox-material-icon-theme
+      ];
+      userSettings = {
+        "update.mode" = "none";
+        "extensions.autoUpdate" = false;
+        "window.titleBarStyle" = "custom";
+        "workbench.colorTheme" = "Gruvbox Dark Hard";
+        "workbench.iconTheme" = "gruvbox-material-icon-theme";
+      };
+      keybindings = [
+        {
+          key = "ctrl+q";
+          command = "editor.action.commentLine";
+          when = "editorTextFocus && !editorReadonly";
+        }
+        {
+          key = "ctrl+s";
+          command = "workbench.action.files.saveFiles";
+        }
+      ];
+    };
   };
 }
