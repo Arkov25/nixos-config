b3d7c2559a45cb2ed067f18f70dd96f1e6b6b008
Arkov25 <Marko.Veljkovic25@gmail.com>
2025-04-05 00:53:33 +0200

    nvidia.nix

--- modules/hardware/nvidia.nix
+++ modules/hardware/nvidia.nix
@@ -1,29 +1,43 @@
+{ config, lib, pkgs, ... }:
 {
-  config,
-  inputs,
-  settings,
-  pkgs,
-  ...
-}:
-{
-  environment.systemPackages = with pkgs; [ cudatoolkit ];
 
-  hardware.graphics.enable = true;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-  services.xserver.videoDrivers = [ "nvidia" ];
+  # Enable OpenGL
+  hardware.graphics = {
+    enable = true;
+  };
 
-  boot.kernelParams = [
-    "nvidia-drm.modeset=1"
-    "nvidia-drm.fbdev=1"
-  ];
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = ["nvidia"];
 
   hardware.nvidia = {
-    powerManagement = {
-      enable = true;
-      finegrained = false;
-    };
-    open = false;
+
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+    # of just the bare essentials.
+    powerManagement.enable = false;
+
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = true;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of 
+    # supported GPUs is at: 
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # Only available from driver 515.43.04+
+    open = true;
+
+    # Enable the Nvidia settings menu,
+	# accessible via `nvidia-settings`.
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
+  ...
 }
