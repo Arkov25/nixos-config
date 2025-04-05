0242040c36be04c72b8cd6aa070efb6a32783a09
Arkov25 <Marko.Veljkovic25@gmail.com>
2025-04-05 22:38:16 +0200

    replace nemo for thunar

--- modules/home/thunar.nix
+++ modules/home/thunar.nix
@@ -1,6 +1,18 @@
-{ pkgs, ... }:
-{
-  home.packages = with pkgs; [
-    thunar
-  ];
+{pkgs, ...}: {
+  programs.thunar = {
+    enable = true;
+    plugins = with pkgs.xfce; [
+      thunar-archive-plugin # Archive management
+      thunar-volman # Volume management (automount removable devices)
+      thunar-media-tags-plugin # Tagging & renaming feature for media files
+    ];
+  };
+  # Archive manager
+  programs.file-roller = {
+    enable = true;
+    package = pkgs.file-roller;
+  };
+  services.tumbler.enable = true; # Thumbnail support for images
 }
+
+#from https://github.com/Sly-Harvey/NixOS
