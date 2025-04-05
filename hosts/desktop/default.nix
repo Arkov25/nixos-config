b04460f577a039516991c3b0db259cad94e4252c
Arkov25 <Marko.Veljkovic25@gmail.com>
2025-04-05 11:36:09 +0200

    Filesystems.nix

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -4,6 +4,7 @@
     ./hardware-configuration.nix
     ./../../modules/core
     ./../../modules/hardware/nvidia.nix
+    ./../../modules/hardware/filesystems.nix
   ];
 
   powerManagement.cpuFreqGovernor = "performance";
---
+++ modules/hardware/filesystems.nix
@@ -0,0 +1,37 @@
+# /etc/nixos/filesystems.nix
+{ config, lib, username, ... }:
+
+{
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-uuid/175a7bf3-630b-45be-bd80-b0d08890f887";
+      fsType = "btrfs";
+      options = [ "subvol=@" "compress=zstd:1" "noatime" ];
+    };
+
+    "/nix" = {
+      device = "/dev/disk/by-uuid/175a7bf3-630b-45be-bd80-b0d08890f887";
+      fsType = "btrfs";
+      options = [ "subvol=@nix" "compress=zstd:1" "noatime" ];
+    };
+
+    "/var/log" = {
+      device = "/dev/disk/by-uuid/175a7bf3-630b-45be-bd80-b0d08890f887";
+      fsType = "btrfs";
+      options = [ "subvol=@var_log" "compress=zstd:1" "noatime" ];
+    };
+
+    "/home" = {
+      device = "/dev/disk/by-uuid/175a7bf3-630b-45be-bd80-b0d08890f887";
+      fsType = "btrfs";
+      options = [ "subvol=@home" "compress=zstd:1" "noatime" ];
+
+    "/home/${username}/Games" = {
+      device = "/dev/disk/by-uuid/175a7bf3-630b-45be-bd80-b0d08890f887";
+      fsType = "btrfs";
+      options = [ "subvol=@home/${username}/Games" "compress=zstd:1" "noatime" ];
+
+    };
+    };
+  }
+}
