455f377b37b23c0001263db50b78378f5f98bd8e
Arkov25 <Marko.Veljkovic25@gmail.com>
2025-04-05 16:21:40 +0200

    discord no vencord

--- modules/home/discord/discord.nix
+++ modules/home/discord/discord.nix
@@ -2,9 +2,9 @@
 {
   home.packages = with pkgs; [
      discord
-     (discord.override {
-      withVencord = true;
-     })
+     #(discord.override {
+      #withVencord = true;
+     #})
     webcord-vencord
   ];
   xdg.configFile."Vencord/themes/gruvbox.theme.css".source = ./gruvbox.css;
