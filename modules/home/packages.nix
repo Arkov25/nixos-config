3f54fa0e9eb72b9998ca9b3a024c2b77f0ea0731
Arkov25 <Marko.Veljkovic25@gmail.com>
2025-04-05 18:19:56 +0200

    new packages

--- modules/home/packages.nix
+++ modules/home/packages.nix
@@ -1,9 +1,23 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    #gimp
-    thunderbird
-    #obs-studio
+    #CLI-utility
+    imv #image viewer
+    pamixer #pulseaudio cli mixer
+    playerctl #controller for media players
+    man-pages #more man pages
+    mpv #video player
+    wget #wget
+    #CLI-fun
+    cbonsai
+    #GUI-Apps
+    bleachbit
     hyprshot
+    libreoffice
+    obs-studio
+    pavucontrol   
+    thunderbird
+    vlc
+    winetricks
   ];
 }
