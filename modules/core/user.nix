f923e930468b1a27a8af1e1b978d6d06a2759d08
Arkov25 <Marko.Veljkovic25@gmail.com>
2025-04-05 20:33:31 +0200

    typo

--- modules/core/user.nix
+++ modules/core/user.nix
@@ -10,7 +10,7 @@
   home-manager = {
     useUserPackages = true;
     useGlobalPkgs = true;
-    extraSpecialArgs = { inherit inputs username host; };zz
+    extraSpecialArgs = { inherit inputs username host; };
     users.${username} = {
       imports =
         if (host == "desktop") then
