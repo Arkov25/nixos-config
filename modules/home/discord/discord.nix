7228921a821708ab1c85245f96d1feff0ea1114d
Arkov25 <Marko.Veljkovic25@gmail.com>
2025-04-05 19:12:08 +0200

    nixcord

--- modules/home/discord/discord.nix
+++ modules/home/discord/discord.nix
@@ -1,7 +1,28 @@
-{ pkgs, lib, ... }:
+{ pkgs, lib, nixcord, ... }:
 
 let
-  vencord = pkgs.vencord; # Stelle sicher, dass vencord verfügbar ist
+  vencord = pkgs.vencord;
+
+  # Beispielhafte Vencord Plugin-Konfiguration via nixcord
+  vencordConfig = nixcord.build {
+    settings = {
+      # Allgemeine Einstellungen
+      "readAllNotifications" = false;
+      "hideBlockedMessages" = false;
+    };
+
+    plugins = {
+      MessageLogger = {
+        enabled = false;
+      };
+      AlwaysAppearOnline = {
+        enabled = false;
+      };
+      FakeNitro = {
+        enabled = false;
+      };
+    };
+  };
 in {
   home.packages = [
     (pkgs.discord.override ({
@@ -10,8 +31,13 @@ in {
       vencord = vencord;
       enableAutoscroll = true;
     }))
-    pkgs.webcord-vencord
+    pkgs.webcord
   ];
 
+  # Plugin Theme
   xdg.configFile."Vencord/themes/gruvbox.theme.css".source = ./gruvbox.css;
+
+  # Vencord Plugin- und Settings-Dateien via nixcord
+  xdg.configFile."Vencord/plugins.json".text = builtins.toJSON vencordConfig.plugins;
+  xdg.configFile."Vencord/settings/settings.json".text = builtins.toJSON vencordConfig.settings;
 }
