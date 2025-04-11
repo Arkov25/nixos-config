89d8cecfdd5a921e73707398c02582d12600c5c9
marko.veljkovic <marko.veljkovic@student.fh-kiel.de>
2025-04-11 10:54:00 +0200

    pipewire exluded from core. Just Desktop and Laptop import it. Audio seems to be not supported on Hyper-V VMs

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -4,6 +4,7 @@
     ./hardware-configuration.nix
     ../../modules/core
     ../../modules/hardware/nvidia.nix
+    ../../modules/hardware/pipewire.nix
     #../../modules/services/default.nix
     #./../../modules/hardware/filesystems.nix
   ];
