a988cda36d92d73f628e4c8b0285591b0636e714
Arkov25 <Marko.Veljkovic25@gmail.com>
2025-04-12 09:11:05 +0200

    import hyprland cachix

--- modules/core/system.nix
+++ modules/core/system.nix
@@ -7,17 +7,19 @@
     experimental-features = [ "nix-command" "flakes" ];
     substituters = [
       "https://nix-gaming.cachix.org"
+      "https://hyprland.cachix.org"
       "https://ghostty.cachix.org"
     ];
     trusted-public-keys = [
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "ghostty.cachix.org-1:QB389yTa6gTyneehvqG58y0WnHjQOqgnA+wBnpWWxns="
     ];
   };
   
   nixpkgs.config.allowUnfree = true;
   
-#  environment.systemPackages = with pkgs; [ wget git (xivlauncher-rb.override { useGameMode = true; }) ];
+  environment.systemPackages = with pkgs; [ wget git (xivlauncher-rb.override { useGameMode = true; }) ];
 
   console.keyMap = "de";
   time.timeZone = "Europe/Berlin";
